5ad8c67f1f1c4ea16bef452bda27c2c88bcae08a
Thayllo Oliveira <thayllo.co@gmail.com>
2020-10-24 14:37:57 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>AAAAAAAAAA</Text>
+      </SafeAreaView>
+    </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
